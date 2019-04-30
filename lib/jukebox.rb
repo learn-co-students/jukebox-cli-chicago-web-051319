56d8a5dfc500a426df7b156ce96b9c6483cd20c5
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-30 04:05:10 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,61 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+
+def help
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"
+end
+
+def play(input)
+  puts input
+  #puts "#{input} is the input"
+  a = ""
+  ec = "Invalid input, please try again"
+  
+  if input.class == Integer && input <= songs.length
+    a = songs[input]
+  elsif input.class == String && songs.include?(input)
+    a = songs[songs.index_of(input)]
+  else
+    a = ec
+  end
+  
+  puts "Playing #{a}"
+  #puts "#{a} is the output"
+  return a
+end
+
+def list(col)
+  puts col
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(col)
+  puts "Please enter a command:"
+  a = ""
+  
+  while a != "exit"
+    if a == "help"
+      help
+    elsif a == "play"
+      puts "Enter Song name or track number:"
+      play(b)
+    elsif a == "list"
+      list(col)  
+    elsif a == "exit"
+      a = "exit"
+      exit_jukebox
+    else
+      puts "Invalid input, type 'help' for commands"
+    end
+  end
+end
+
+run(songs)
